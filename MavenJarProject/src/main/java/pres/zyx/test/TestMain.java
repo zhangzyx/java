af711bfa90fe3142d6438769b84989cdfca15503
zhangzyx <zhangzyx1984@126.com>
2017-04-22 00:08:07 +0800

    新增MavenJarProject

---
+++ MavenJarProject/src/main/java/pres/zyx/test/TestMain.java
@@ -0,0 +1,12 @@
+package pres.zyx.test;
+
+import org.apache.commons.lang.CharUtils;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.println(CharUtils.toChar("TestMain111"));
+		System.out.println("------------------------------------------");
+	}
+
+}
---
+++ MavenJarProject/src/main/java/pres/zyx/test/TestMain2.java
@@ -0,0 +1,12 @@
+package pres.zyx.test;
+
+import org.apache.commons.lang.CharUtils;
+
+public class TestMain2 {
+
+	public static void main(String[] args) {
+		System.out.println(CharUtils.toChar("TestMain222"));
+		System.out.println("------------------------------------------");
+	}
+
+}
