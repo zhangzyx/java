b99d8f2e0240c8df17b2892d043a114eeab8161f
zhangzyx <zhangzyx1984@126.com>
2017-04-22 23:48:00 +0800

    class scan

--- ClassScan/src/main/java/prev/zyx/lib/classscan/utils/FileUtil.java
+++ ClassScan/src/main/java/prev/zyx/lib/classscan/utils/FileUtil.java
@@ -1,13 +1,22 @@
 package prev.zyx.lib.classscan.utils;
 
 import java.io.File;
+import java.io.FileFilter;
 
 public class FileUtil {
-	public static File[] filterSurffixFiles(String path, String surffix) {
+	public static File[] filterSurffixFiles(String path, final String surffix) {
 		if (path == null || "".equals(path)) {
 			return null;
 		}
 		
-		return new File(path).listFiles(new ClassScanFileFilter(surffix));
+		//return new File(path).listFiles(new ClassScanFileFilter(surffix));
+		return new File(path).listFiles(new FileFilter() {
+
+			@Override
+			public boolean accept(File file) {
+				return (file.isFile() && file.getName().endsWith(surffix)) || file.isDirectory();
+			}
+			
+		});
 	}
 }
